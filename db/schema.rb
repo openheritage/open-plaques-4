9e0e860b4841bb653de79fb5b92752cacd33ab59
Jez Nicholson <jez.nicholson@gmail.com>
2025-12-21 12:56:35 +0000

    author might not be known

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "created_at", null: false
     t.string "name", null: false
     t.bigint "record_id", null: false
     t.string "record_type", null: false
@@ -29,7 +29,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.string "content_type"
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "created_at", null: false
     t.string "filename", null: false
     t.string "key", null: false
     t.text "metadata"
@@ -38,14 +38,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "areas", id: :serial, force: :cascade do |t|
     t.integer "country_id"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "dbpedia_uri", limit: 255
     t.float "latitude"
     t.float "longitude"
     t.string "name", limit: 255
     t.integer "plaques_count"
     t.string "slug", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["country_id"], name: "index_areas_on_country_id"
     t.index ["name"], name: "index_areas_on_name"
     t.index ["slug"], name: "index_areas_on_slug"
@@ -53,33 +53,33 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "colours", id: :serial, force: :cascade do |t|
     t.boolean "common", default: false, null: false
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "dbpedia_uri", limit: 255
     t.string "name", limit: 255
     t.integer "plaques_count"
     t.string "slug", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["slug"], name: "index_colours_on_slug"
   end
 
   create_table "countries", id: :serial, force: :cascade do |t|
     t.string "alpha2", limit: 255
     t.integer "areas_count"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
     t.float "latitude"
     t.float "longitude"
     t.string "name", limit: 255
     t.integer "plaques_count"
     t.integer "preferred_zoom_level"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "wikidata_id"
   end
 
   create_table "google_analytics", force: :cascade do |t|
     t.float "average_time_on_page"
     t.float "bounce_rate"
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "created_at", null: false
     t.bigint "entrances"
     t.float "exit_percentage"
     t.string "page", null: false
@@ -93,24 +93,24 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "languages", id: :serial, force: :cascade do |t|
     t.string "alpha2", limit: 255
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "name", limit: 255
     t.integer "plaques_count"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
   end
 
   create_table "licences", id: :serial, force: :cascade do |t|
     t.string "abbreviation", limit: 255
     t.boolean "allows_commercial_use"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "name", limit: 255
     t.integer "photos_count"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "url", limit: 255
   end
 
   create_table "organisations", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
     t.integer "language_id"
     t.float "latitude"
@@ -119,7 +119,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.text "notes"
     t.string "slug", limit: 255
     t.integer "sponsorships_count", default: 0
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "website", limit: 255
     t.index ["name"], name: "index_organisations_on_name"
     t.index ["slug"], name: "index_organisations_on_slug"
@@ -129,11 +129,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.text "abstract"
     t.bigint "author_id"
     t.text "body"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "name", limit: 255
     t.string "slug", limit: 255
     t.string "strapline", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["author_id"], name: "index_pages_on_author_id"
   end
 
@@ -143,7 +143,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.date "born_on"
     t.boolean "born_on_is_circa"
     t.string "citation"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.date "died_on"
     t.boolean "died_on_is_circa"
     t.string "ethnicity"
@@ -155,7 +155,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.integer "personal_connections_count"
     t.integer "personal_roles_count"
     t.string "surname_starts_with", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "wikidata_id"
     t.index ["born_on", "died_on"], name: "born_and_died"
     t.index ["index"], name: "index_people_on_index"
@@ -163,13 +163,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   end
 
   create_table "personal_connections", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
-    t.datetime "ended_at", precision: nil
+    t.datetime "created_at"
+    t.datetime "ended_at"
     t.integer "person_id"
     t.integer "plaque_connections_count"
     t.integer "plaque_id"
-    t.datetime "started_at", precision: nil
-    t.datetime "updated_at", precision: nil
+    t.datetime "started_at"
+    t.datetime "updated_at"
     t.integer "verb_id"
     t.index ["person_id"], name: "index_personal_connections_on_person_id"
     t.index ["plaque_id"], name: "index_personal_connections_on_plaque_id"
@@ -177,7 +177,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   end
 
   create_table "personal_roles", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.date "ended_at"
     t.integer "ordinal"
     t.integer "person_id"
@@ -185,7 +185,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.integer "related_person_id"
     t.integer "role_id"
     t.date "started_at"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["person_id"], name: "index_personal_roles_on_person_id"
     t.index ["related_person_id"], name: "index_personal_roles_on_related_person_id"
     t.index ["role_id"], name: "index_personal_roles_on_role_id"
@@ -193,7 +193,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "photos", id: :serial, force: :cascade do |t|
     t.integer "clone_id"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
     t.integer "distance_to_nearest_plaque"
     t.string "file_url", limit: 255
@@ -208,9 +208,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.integer "plaque_id"
     t.string "shot", limit: 255
     t.string "subject", limit: 255
-    t.datetime "taken_at", precision: nil
+    t.datetime "taken_at"
     t.string "thumbnail", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "url", limit: 255
     t.index ["licence_id"], name: "index_photos_on_licence_id"
     t.index ["person_id"], name: "index_photos_on_person_id"
@@ -219,21 +219,21 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   end
 
   create_table "picks", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
-    t.datetime "feature_on", precision: nil
+    t.datetime "feature_on"
     t.integer "featured_count"
-    t.datetime "last_featured", precision: nil
+    t.datetime "last_featured"
     t.integer "plaque_id"
     t.string "proposer", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
   end
 
   create_table "plaques", id: :serial, force: :cascade do |t|
     t.string "address", limit: 255
     t.integer "area_id"
     t.integer "colour_id"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
     t.date "erected_at"
     t.text "inscription"
@@ -251,7 +251,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.string "reference", limit: 255
     t.integer "series_id"
     t.string "series_ref", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["area_id"], name: "index_plaques_on_area_id"
     t.index ["colour_id"], name: "index_plaques_on_colour_id"
     t.index ["latitude", "longitude"], name: "geo"
@@ -260,7 +260,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "roles", id: :serial, force: :cascade do |t|
     t.string "abbreviation", limit: 255
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.text "description"
     t.string "index", limit: 255
     t.string "name", limit: 255
@@ -270,7 +270,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.string "role_type", limit: 255
     t.string "slug", limit: 255
     t.string "suffix", limit: 255
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "wikidata_id"
     t.index ["index"], name: "starts_with"
     t.index ["role_type"], name: "index_roles_on_role_type"
@@ -278,20 +278,20 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   end
 
   create_table "series", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "description", limit: 255
     t.float "latitude"
     t.float "longitude"
     t.string "name", limit: 255
     t.integer "plaques_count"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
   end
 
   create_table "sponsorships", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.integer "organisation_id"
     t.integer "plaque_id"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.index ["organisation_id"], name: "index_sponsorships_on_organisation_id"
     t.index ["plaque_id"], name: "index_sponsorships_on_plaque_id"
   end
@@ -323,11 +323,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
 
   create_table "todo_items", id: :serial, force: :cascade do |t|
     t.string "action", limit: 255
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "description", limit: 255
     t.string "image_url", limit: 255
     t.integer "plaque_id"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "url", limit: 255
     t.integer "user_id"
   end
@@ -335,9 +335,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   create_table "users", id: :serial, force: :cascade do |t|
     t.text "biography"
     t.string "bluesky"
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "crypted_password", limit: 40
-    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "current_sign_in_at"
     t.string "current_sign_in_ip", limit: 255
     t.string "email", limit: 100
     t.string "encrypted_password", limit: 128, null: false
@@ -346,7 +346,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.string "instagram"
     t.boolean "is_admin"
     t.boolean "is_verified", default: false, null: false
-    t.datetime "last_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at"
     t.string "last_sign_in_ip", limit: 255
     t.string "linkedin"
     t.string "mastodon"
@@ -355,9 +355,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.string "photo_uri"
     t.string "provider"
     t.string "refresh_token"
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "remember_token_expires_at", precision: nil
-    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at"
+    t.datetime "remember_token_expires_at"
+    t.datetime "reset_password_sent_at"
     t.string "reset_password_token", limit: 255
     t.string "salt", limit: 40
     t.integer "sign_in_count", default: 0
@@ -365,7 +365,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
     t.string "token"
     t.string "twitter"
     t.string "uid"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
     t.string "username", limit: 40
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid"
@@ -374,10 +374,10 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_085727) do
   end
 
   create_table "verbs", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil
+    t.datetime "created_at"
     t.string "name", limit: 255
     t.integer "personal_connections_count"
-    t.datetime "updated_at", precision: nil
+    t.datetime "updated_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
