f4bea981503626536c0c4446bbd822912439ce13
Jez Nicholson <jez.nicholson@gmail.com>
2026-02-24 16:51:09 +0000

    update javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,25 +5,26 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "bootstrap" # @5.3.6
+pin "bootstrap" # @5.3.8
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "aos" # @2.3.4
-pin "chart.js" # @4.4.9
-pin "kurklecolor" # https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.4/+esm
+# pin "kurklecolor" # https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.4/+esm
 pin "@stimulus-components/chartjs", to: "@stimulus-components--chartjs.js" # @6.0.1
-pin "@stimulus-components/clipboard", to: "stimulus-clipboard.js" # @4.0.1
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "desandro-matches-selector" # @2.0.2
-pin "ev-emitter" # @1.1.1
-pin "fizzy-ui-utils" # @2.0.7
-pin "get-size" # @2.0.3
+pin "ev-emitter" # @2.1.2
+pin "fizzy-ui-utils" # @3.0.0
+pin "get-size" # @3.0.0
 pin "masonry-layout" # @4.2.2
 pin "outlayer" # @2.1.1
-pin "swiper" # @12.0.3 https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js
+pin "swiper" # @12.1.12 https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js
 pin "zenblog"
 pin "stimulus-autocomplete" # @3.1.0
 pin "lodash" # @4.17.23
 pin "lodash.debounce" # @4.0.8
 pin "layer-switcher"
-pin "maplibre-gl" # @5.15.0
+pin "maplibre-gl" # @5.18.0
+pin "chart.js" # @4.5.1
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+pin "@chartjs/helpers-dataset", to: "chunks/helpers.dataset.js"
