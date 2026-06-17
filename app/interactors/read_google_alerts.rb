f82627c109da833a6d7bcb5828ebcf71b31259fc
Jez Nicholson <jez.nicholson@gmail.com>
2026-06-17 15:37:51 +0100

    Add rake task to be called by Heroku scheduler

--- app/interactors/read_google_alerts.rb
+++ app/interactors/read_google_alerts.rb
@@ -4,9 +4,6 @@ class ReadGoogleAlerts
   def call
     feed = RSS::Parser.parse("https://www.google.com/alerts/feeds/09563737050033496915/15988312346396900564")
     feed.items.each do |item|
-      # puts "name: #{item.title.content}"
-      # puts "url: #{item.link.href}"
-      # puts "description: #{item.content.content}"
       TodoItem.create!(action: "google_alert", description: item.content.content, name: item.title.content, url: item.link.href) unless TodoItem.find_by(action: "google_alert", name: item.title.content)
     end
   end
