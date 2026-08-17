23b1a8c734c583118e98f6a351688848f62d06ad
Jez Nicholson <jez.nicholson@gmail.com>
2026-08-17 11:52:01 +0100

    ignore nils in min max calculations

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -3,10 +3,10 @@ class CountiesController < ApplicationController
   def show
     @county = params[:id]
     @areas = Area.county(@county)
-    sw_corner = [ @areas.pluck(:min_longitude).min, @areas.pluck(:min_latitude).min ]
-    ne_corner = [ @areas.pluck(:max_longitude).max, @areas.pluck(:max_latitude).max ]
+    sw_corner = [ @areas.pluck(:min_longitude).compact.min, @areas.pluck(:min_latitude).compact.min ]
+    ne_corner = [ @areas.pluck(:max_longitude).compact.max, @areas.pluck(:max_latitude).compact.max ]
     @bbox_array = [ sw_corner, ne_corner ]
-    @center_point_array = [ @areas.pluck(:longitude).max, @areas.pluck(:latitude).max ]
+    @center_point_array = [ @areas.pluck(:longitude).compact.max, @areas.pluck(:latitude).compact.max ]
     @plaques = Plaque.where(area_id: @areas.map(&:id))
     @plaques_count = @plaques.count # size is 0
     @uncurated_count = @plaques.unconnected.size
