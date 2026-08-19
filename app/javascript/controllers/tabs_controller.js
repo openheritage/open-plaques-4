d3a87f8d9dbdb699a891f23ba2215eeb287fb5de
Jez Nicholson <jez.nicholson@gmail.com>
2026-08-19 15:22:05 +0100

    refactor tabs in Stimulus controller

---
+++ app/javascript/controllers/tabs_controller.js
@@ -0,0 +1,29 @@
+// app/javascript/controllers/tab_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["tab", "panel"]
+
+  connect() {
+    this.show(this.tabTargets[0])
+  }
+
+  change(event) {
+    event.preventDefault()
+    this.show(event.currentTarget)
+  }
+
+  show(selectedTab) {
+    const targetId = selectedTab.dataset.target
+
+    this.tabTargets.forEach(tab => {
+      tab.classList.toggle("active", tab === selectedTab)
+    })
+
+    this.panelTargets.forEach(panel => {
+      panel.classList.toggle("show", panel.id === targetId)
+      panel.classList.toggle("active", panel.id === targetId)
+      panel.hidden = panel.id !== targetId
+    })
+  }
+}
