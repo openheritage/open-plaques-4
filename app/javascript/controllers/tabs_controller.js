54be671647c31e78d202447b4e5409fef1bdcbec
Jez Nicholson <jez.nicholson@gmail.com>
2026-09-02 11:59:57 +0100

    controller name comments

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,4 +1,4 @@
-// app/javascript/controllers/tab_controller.js
+// app/javascript/controllers/tabs_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
